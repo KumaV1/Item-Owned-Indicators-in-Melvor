faa2b38a85735687c4f9f5fb636e68f32cec958d
KumaV1 <KumaV1>
2024-04-09 19:05:00 +0200

    Initial implementation

---
+++ src/helpers/CombatLootUiHelper.ts
@@ -0,0 +1,9 @@
+export class CombatLootUiHelper {
+    public static createBadge(prefix: string, qty: number): string {
+        if (qty < 1) {
+            return '';
+        }
+
+        return `<div class="text-center"><span class="badge badge-pill m-1 badge-success">${prefix}: ${formatNumber(qty)}</span></div>`;
+    }
+}
